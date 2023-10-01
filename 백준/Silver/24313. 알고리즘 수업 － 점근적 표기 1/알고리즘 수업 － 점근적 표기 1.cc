19b689c16abcbfd21dd534ec5866cc4a443e27fa
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2023-10-01 16:22:09 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int a1, a0, c, n0, cnt = 0;
+
+	cin >> a1 >> a0 >> c >> n0;
+
+	if ((a1 * n0 + a0) <= (c * n0) && (a1 <= c))
+		cout << "1";
+	else
+		cout << "0";
+
+}
