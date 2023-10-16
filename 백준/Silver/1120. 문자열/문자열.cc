f87e16e1151eef279a201f210495b9e3cb60eea0
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2023-10-16 15:04:05 +0900

    [Silver IV] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.cc
+++ 백준/Silver/1120. 문자열/문자열.cc
@@ -6,24 +6,15 @@
 using namespace std;
 
 int main() {
-	string str1, str2, min_str, max_str;
+	string str1, str2;
 	vector <int>v;
 
 	cin >> str1 >> str2;
 
-	if (str1.length() < str2.length()) {
-		min_str = str1;
-		max_str = str2;
-	}
-	else {
-		min_str = str2;
-		max_str = str1;
-	}
-
-	for (int i = 0; i <= max_str.length() - min_str.length(); i++) {
+	for (int i = 0; i <= str2.length() - str1.length(); i++) {
 		int count = 0;
-		for (int j = 0; j < min_str.length(); j++) {
-			if (max_str[i + j] != min_str[j])
+		for (int j = 0; j < str1.length(); j++) {
+			if (str2[i + j] != str1[j])
 				count++;
 		}
 		v.push_back(count);
