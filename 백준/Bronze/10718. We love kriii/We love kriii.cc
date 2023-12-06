e95cdc4afabdb215839f82f39c2062776982d4cc
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2023-12-06 21:12:57 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main(int argc, char const *argv[]) {
+ 
+    std::cout << "강한친구 대한육군\n강한친구 대한육군";
+    /*
+    또는 
+    std::cout << "강한친구 대한육군\n";
+    std::cout << "강한친구 대한육군";
+    */
+    return 0;
+}
