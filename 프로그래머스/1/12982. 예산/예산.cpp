56f748a30470ff0df6af77d0956552696b4af21b
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-09-05 00:53:57 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    int money = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for(int i=0; i<d.size(); i++){
+        if(money+d[i] <= budget){
+            money += d[i];
+            answer++;
+        }
+        else{
+            break;
+        }
+    }
+    
+    return answer;
+}
