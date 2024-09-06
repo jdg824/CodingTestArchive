adfe90d068e821972408797cc0baa777e3d9b7d9
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-09-06 10:21:37 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,38 +1,37 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    queue<int> q;
     int answer = 0;
-    int max = *max_element(priorities.begin(), priorities.end());
+    queue<int> q;
     
-    for(int i =0; i< priorities.size(); i++)
+    for(int i=0; i<priorities.size(); i++){
         q.push(i);
-    
-    int count = 1;
-    while(1){
-        int num = q.front();
-        
-        if(priorities[num] < max){
-            q.push(num);
-            q.pop();
-        }
-        else{
-            if(num == location)
-                return count;
-            
-            priorities[num] = 0;
-            max = *max_element(priorities.begin(), priorities.end());
-            count++;
-            q.pop();
-        }
     }
     
+    int max = *max_element(priorities.begin(), priorities.end());
+    int count = 1;
+    
+    while(1){
+        int idx = q.front();
+        
+        if(priorities[idx] < max){
+            q.pop();
+            q.push(idx);
+        }
+        else{
+            if(idx == location)
+                return count;
+            
+            priorities[idx] = 0;
+            count++;
+            max = *max_element(priorities.begin(), priorities.end());
+        }
+    }
     
     return answer;
 }
