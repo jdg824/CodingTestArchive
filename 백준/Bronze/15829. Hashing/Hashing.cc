7d4c9e08f28e0879eeabf3d8974cf01e21fc761c
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-09-24 20:14:05 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -7,11 +7,13 @@ int main() {
 	int l;	//길이
 	string str;
 	long long ans = 0;
+	long long d = 1;
 
 	cin >> l >> str;
-
+	
 	for (int i = 0; i < l; i++) {
-		ans += ((str[i] - 'a')+1) * pow(31, i);
+		ans = ans + (long((str[i]) - 96) * d) % 1234567891;
+		d = (d * 31) % 1234567891;
 	}
 
 	cout << ans % 1234567891;
