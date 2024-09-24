903a976ed86e55b3c1c24902a262f47757447196
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-09-24 11:48:47 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int num) {
+	if (num == 0 || num == 1)
+		return 1;
+	else
+		return fact(num - 1) * num;
+}
+
+int main() {
+	int n, k;
+
+	cin >> n >> k;
+
+	cout << fact(n)/(fact(n-k)*fact(k));
+}
