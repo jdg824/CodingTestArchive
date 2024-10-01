4e5824db13ddda950c43cef946531c766fcd0d3a
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-01 21:05:20 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 7796 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -19,9 +19,19 @@ int main() {
 
 	int cnt = 0;
 
-	for (int i = 0; i <= m - cmp.size(); i++) {
-		if (str.substr(i, cmp.size()) == cmp)
-			cnt++;
+	for (int i = 0; i <= m; i++) {
+		int chk = 0;
+		if (str[i] == 'I') {
+			while (str[i + 1] == 'O' && str[i + 2] == 'I') {
+				chk++;
+				if (chk == n) {
+					chk--;
+					cnt++;
+				}
+				i += 2;
+			}
+			chk = 0;
+		}
 	}
 
 	cout << cnt;
