8f6f6f721e8aff78308e07f47ea0096b2f08b388
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-03 03:02:26 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    string tmp ="";
+    for(int i=0; i<my_string.size(); i++){
+        if(my_string[i] == ' '){
+            if(tmp != ""){
+                answer.push_back(tmp);  
+                tmp = "";
+            }
+        }
+        else
+            tmp += my_string[i];
+    }
+    
+    if(tmp != "")
+        answer.push_back(tmp);
+    
+    return answer;
+}
