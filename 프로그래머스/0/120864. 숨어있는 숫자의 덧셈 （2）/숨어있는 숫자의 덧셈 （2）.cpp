16850e788bb85c007d67bd37be44f4d31d117450
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-05 22:49:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    
+    string str = "";
+    for(int i=0; i<my_string.size(); i++){
+        if(isdigit(my_string[i]))
+            str+=my_string[i];
+        else{
+            if(str!=""){
+                answer += stoi(str);
+                str ="";   
+            }
+        }
+    }
+    
+    if(str != "")
+        answer += stoi(str);
+    
+    return answer;
+}
