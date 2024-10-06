950cdce1a4f6f55c28c1121fd86def4357e462ba
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-06 21:55:55 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int v[9] = { 0, };
+bool chk[9] = { 0, };
+
+void dfs(int num, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = num; i <= n; i++) {
+			if (chk[i] == 0) {
+				chk[i] = 1;
+				v[cnt] = i;
+				dfs(i, cnt + 1);
+				chk[i] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+	dfs(1, 0);
+
+	return 0;
+}
