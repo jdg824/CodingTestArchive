d9b11d6dbbc5030c5c55855a779eb1e22ddcf005
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-07 23:01:21 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -1,19 +1,27 @@
 #include <string>
 #include <vector>
-#include <math.h>
 
 using namespace std;
 
-double fact(int num){
-    if(num<=1)
-        return 1;
-    else
-        return num * fact(num-1);
+int num = 0;
+int chk[31] = {0,};
+
+int dfs(int num, int cnt, int balls, int share){
+    if(cnt == share)
+       num++;
+    else{
+        for(int i=num; i<=balls; i++){
+            if(chk[cnt] == 0){
+                chk[cnt] = 1;
+                dfs(i, cnt+1, balls, share);
+                chk[cnt] = 0;
+            }
+        }
+    }
 }
 
 int solution(int balls, int share) {
-    double ans = fact(balls) / (fact(balls-share) * fact(share));
-        
-    return round(ans);
+    
+    return dfs(1, 0, balls, share);
     
 }
