6fa5f1e2091c6de179ce6f65f3b9d2dcfb86a6bd
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-09 01:11:21 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.26 ms, Memory: 3.78 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    priority_queue<int, vector<int>, greater<>> pq;
+    
+    for(int i=0; i<score.size(); i++){
+        if(pq.size() < k)
+            pq.push(score[i]);
+        else{
+            if(score[i] > pq.top()){
+                pq.pop();
+                pq.push(score[i]);   
+            }
+        }
+        answer.push_back(pq.top());
+    }
+    
+    return answer;
+}
