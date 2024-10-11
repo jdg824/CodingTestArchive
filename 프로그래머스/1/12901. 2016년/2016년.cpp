5e6bbbc400178d3bf0ca91da6858bdcc606805cb
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-11 23:30:36 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int month[13] = {0,31,29,31,30,31,30, 31, 31, 30,31,30,31};
+string day[7] = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+
+string solution(int a, int b) {
+    int allday = 0;
+    string answer = "";
+    
+    for(int i=1; i<=a-1; i++)
+        allday += month[i];
+    allday += b;
+    
+    
+    
+    answer = day[allday%7];
+    
+    return answer;
+}
