a58907c65ac6e114a4088916c3dc8757779a0eaf
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-15 00:25:40 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    
+    for(int i=1; i<n; i++){
+        int sum = i;
+        for(int j=i+1; j<n; j++){
+            sum+=j;
+            if(sum > n)
+                break;
+            else if(sum == n){
+                answer++;
+                break;
+            }
+                
+        }
+    }
+    return answer;
+}
