077a93dbc004b16eb6df8b61d978172a550973f6
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2024-10-23 22:09:44 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.28 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dp[60001] = {0,};
+
+int solution(int n) {
+    int answer = 0;
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i=3; i<=n; i++){
+        dp[i] = (dp[i-2]+ dp[i-1])%1000000007;
+    }
+    return dp[n];
+}
