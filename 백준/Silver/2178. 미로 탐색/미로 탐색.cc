2bbd81cf59556c4fbbe75095ec3af34d0d048508
DongGyu Jung <80143957+jdg824@users.noreply.github.com>
2026-04-08 22:05:36 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,21 +3,20 @@
 
 using namespace std;
 
-int arr[102][102] = { 0, };
-bool chk[102][102] = { 0, };
-int graph[102][102] = { 0, };
+int graph[101][101] = { 0, };
+int road[101][101] = { 0, };
+bool chk[101][101] = { 0, };
+
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
 
 int n, m;
 
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { 1,-1,0,0 };
+void bfs(int x, int y) {
+	queue<pair<int, int>>q;
 
-void bfs(int sx, int sy) {
-	queue<pair<int, int>> q;
-
-	chk[sx][sy] = 1;
-	graph[sx][sy] = 1;
-	q.push({ sx, sy });
+	q.push({ x,y });
+	chk[x][y] = 1;
 
 	while (!q.empty()) {
 		int rx = q.front().first;
@@ -28,26 +27,29 @@ void bfs(int sx, int sy) {
 			int px = rx + dx[i];
 			int py = ry + dy[i];
 
-			if (px >= 1 && px <= n && py >= 1 && py <= m && arr[px][py] == 1 && chk[px][py] == 0) {
-				//cout << px << " " << py << "\n";
+			if (px >= 0 && px <= n && py >= 0 && py <= m && chk[px][py] == 0 && graph[px][py] == 1) {
+				q.push({ px,py });
 				chk[px][py] = 1;
-				q.push({ px, py });
+
 				graph[px][py] = graph[rx][ry] + 1;
 			}
 		}
 	}
+
 }
 
 int main() {
 	cin >> n >> m;
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			scanf("%1d", &arr[i][j]);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%1d", &graph[i][j]);
 		}
 	}
-	
-	bfs(1, 1);
 
-	cout << graph[n][m];
+	bfs(0, 0);
+
+	cout << graph[n - 1][m - 1];
+
+	return 0;
 }
